70c47ebc909b4724fe2b4d1d722dd37a511c4593
ValeroniceK <v.kosmach11@gmail.com>
2026-07-07 22:52:12 +0300

    Наращивание тестовой базы и рефакторинг

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,6 @@
-from playwright.sync_api import Page
+from re import Pattern
+from playwright.sync_api import Page, expect
+
 
 class BasePage:
     def __init__(self, page: Page):
@@ -6,3 +8,9 @@ class BasePage:
 
     def visit(self, url: str):
         self.page.goto(url, wait_until='networkidle')
+
+    def reload(self):
+        self.page.reload(wait_until='domcontentloaded')
+
+    def check_current_url(self, expected_url: Pattern[str]):
+        expect(self.page).to_have_url(expected_url)
